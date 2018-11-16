e4d1a2c538d243df62e6a0ed6d41ca00be498d38
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2018-11-16 08:57:32 -0500

    FINISHEDgit add .!

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 
 const app = express();
 
+var pageA__views = 0;
+var pageB__views = 0;
+var pageC__views = 0;
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
@@ -33,15 +37,24 @@ app.post('/api/count', function (request, response) {
     var url = request.body.url;
 
     if (url === 'pageA') {
-        
-
+        pageA__views += 1;
+        console.log(pageA__views);
     } else if (url === 'pageB') {
-
+        pageB__views += 1;
+        console.log(pageB__views)
     } else if (url === 'pageC') {
-
+        pageC__views += 1;
+        console.log(pageC__views);
     } else {
 
     }
+
+    var txt = [];
+    txt[0] = 'Página A: ' + pageA__views + ' visitas   ';
+    txt[1] = 'Página B: ' + pageB__views + ' visitas   ';
+    txt[2] = 'Página C: ' + pageC__views + ' visitas   ';
+
+    fs.writeFileSync("TXT/visitas.txt", txt);
 });
 
 app.listen(5500);
